09b177ac0cfee00f54e08619ddea78ff6f55dba2
Sergii Kutnii <mnkutster@gmail.com>
2018-01-25 15:58:12 +0200

    Project created

---
+++ RingLabsTest/RingLabsTest/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  RingLabsTest
+//
+//  Created by Serge Kutny on 1/25/18.
+//  Copyright © 2018 skutnii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    
+    override func loadView() {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
